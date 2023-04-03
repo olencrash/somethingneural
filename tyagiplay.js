bfebc06616bd4118a8ec200412187dbbc20b21b9
olencrash <129688879+olencrash@users.noreply.github.com>
2023-04-04 00:19:33 +0700

    Update tyagiplay.js

--- tyagiplay.js
+++ tyagiplay.js
@@ -1,1 +1,23 @@
+<script>
+  const firstButton = document.querySelector('.first');
+  const audio = new Audio('https://github.com/olencrash/somethingneural/blob/1a781afb5b9c63b3dbcc64370c1c44cb015f857a/keftemecall.mp3');
 
+  let isPlaying = false;
+
+  firstButton.addEventListener('click', () => {
+    if (!isPlaying) {
+      audio.play();
+      isPlaying = true;
+      const player = document.createElement('audio');
+      player.src = 'https://github.com/olencrash/somethingneural/blob/1a781afb5b9c63b3dbcc64370c1c44cb015f857a/keftemecall.mp3';
+      player.controls = true;
+      player.style.marginTop = '10px';
+      firstButton.parentNode.insertBefore(player, firstButton.nextSibling);
+    } else {
+      audio.pause();
+      isPlaying = false;
+      const player = document.querySelector('audio');
+      player.parentNode.removeChild(player); 
+    }
+  });
+</script>
