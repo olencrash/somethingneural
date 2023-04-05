bf4cad09e3b6188bbd2ff7ef979e823daeadc213
olencrash <129688879+olencrash@users.noreply.github.com>
2023-04-06 00:48:26 +0700

    Update chat.php

--- chat.php
+++ chat.php
@@ -1,27 +1,9 @@
 <?php
-$servername = "sql7.freesqldatabase.com";
-$username = "sql7611364";
-$password = "3bi3v1jHS5";
-$dbname = "chatlog.sql";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Ошибка подключения к серверу: " . $conn->connect_error);
-}
-
-if(isset($_POST['submit'])) {
-    $nickname = $_POST['nickname'];
-    $message = $_POST['message'];
-    
-    $sql = "INSERT INTO chatlog (nickname, message) VALUES ('$nickname', '$message')";
-    
-    if ($conn->query($sql) === TRUE) {
-        echo "Данные успешно записаны в базу данных";
-    } else {
-        echo "Ошибка: " . $sql . "<br>" . $conn->error;
-    }
-}
-
-$conn->close();
+   $conn = new mysqli("sql7.freesqldatabase.com", "sql7611364", "3bi3v1jHS5", "chatlog");
+  
+   if($conn) {
+       echo "успех";
+   }
+   
 ?>
